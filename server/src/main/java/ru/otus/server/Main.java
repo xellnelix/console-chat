42db5b3ef1bf399efc2488f244acc938dd644945
Xellnelix <xellnelix@gmail.com>
2023-10-07 15:58:58 +0300

    Added database

--- server/src/main/java/ru/otus/server/Main.java
+++ server/src/main/java/ru/otus/server/Main.java
@@ -1,8 +1,15 @@
 package ru.otus.server;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(8080, new InMemoryAuthenticationProvider());
-        server.start();
+        try {
+            Server server = new Server(8080);
+            server.start();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
